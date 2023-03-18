28c810d9d4aa2e7bd5af6d926907b5266bf3988a
wongsakron <6310611006@student.tu.ac.th>
2023-03-18 19:55:55 +0700

    initial commit

--- app/src/main/java/com/example/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/example/numberguessinggame/MainActivity.kt
@@ -1,63 +1,119 @@
 package com.example.numberguessinggame
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.numberguessinggame.ui.theme.NumberGuessingGameTheme
+import java.text.NumberFormat
 import kotlin.random.Random
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var guessEditText: EditText
-    private lateinit var guessButton: Button
-    private lateinit var resultTextView: TextView
-    private lateinit var newGameButton: Button
-
-    private var randomNumber: Int = 0
-    private var numGuesses: Int = 0
-
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        guessEditText = findViewById(R.id.guessEditText)
-        guessButton = findViewById(R.id.guessButton)
-        resultTextView = findViewById(R.id.resultTextView)
-        newGameButton = findViewById(R.id.newGameButton)
-
-        newGameButton.setOnClickListener {
-            startNewGame()
-        }
-
-        startNewGame()
-
-        guessButton.setOnClickListener {
-            val guess = guessEditText.text.toString().toIntOrNull()
-
-            if (guess != null) {
-                numGuesses++
-
-                if (guess == randomNumber) {
-                    resultTextView.text = "Correct! You guessed the number in $numGuesses guesses."
-                    newGameButton.visibility = Button.VISIBLE
-                } else if (guess < randomNumber) {
-                    resultTextView.text = "Too low! Guess higher."
-                } else {
-                    resultTextView.text = "Too high! Guess lower."
+        setContent {
+            NumberGuessingGameTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    NumberGuessingGame()
                 }
-            } else {
-                resultTextView.text = "Please enter a valid number."
             }
-
-            guessEditText.text.clear()
         }
-    }
-
-    private fun startNewGame() {
-        numGuesses = 0
-        randomNumber = Random.nextInt(1, 101)
-        resultTextView.text = ""
-        newGameButton.visibility = Button.GONE
     }
 }
+
+@Composable
+fun NumberGuessingGame() {
+    var guess by remember { mutableStateOf("") }
+    var answer by remember { mutableStateOf(Random.nextInt(1, 101)) }
+    var message by remember { mutableStateOf("Guess a number between 1 and 100") }
+    val focusRequester = remember { FocusRequester() }
+    var count by remember { mutableStateOf(0)}
+
+
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = message, modifier = Modifier.padding(bottom = 16.dp))
+
+        OutlinedTextField(
+            value = guess,
+            onValueChange = { guess = it },
+            label = { Text("Guess a number") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            modifier = Modifier.focusRequester(focusRequester)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(onClick = {
+            if (guess.isNotEmpty()) {
+                val number = guess.toInt()
+                when {
+                    number < answer -> {
+                        message = "Your guess is too low"
+                        count = count + 1
+                        guess = ""
+                    }
+                    number > answer -> {
+                        message = "Your guess is too high"
+                        count = count + 1
+                        guess = ""
+                    }
+                    else -> {
+                        message = "You guessed it! The answer was $answer and You guessed the number in $count guesses."
+                    }
+                }
+
+
+                focusRequester.requestFocus()
+            }
+            else {
+                message = "Please enter a valid number."
+            }
+        }) {
+            Text(text = "Guess")
+        }
+        Button(onClick = {
+            guess = ""
+            message = "Guess a number between 1 and 100"
+            answer = Random.nextInt(1, 101)
+            count = 0
+            focusRequester.requestFocus()
+        }) {
+            Text(text = "Reset")
+        }
+    }
+}
+
+
+
+
+
